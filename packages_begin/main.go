6861dddf7ba821dd2e29f551720b84394ffe498d
oleh.skalozub <oleh.skalozub@yalantis.net>
2022-11-19 10:50:28 +0200

    package

---
+++ packages_begin/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// вивід інформації в консоль.
+	fmt.Println("Привіт подвійна шаурма!")
+
+	// вивід поточної дати в консоль.
+	fmt.Println(time.Now())
+}
